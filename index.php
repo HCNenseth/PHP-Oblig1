ecf997dd84aec7fa2bb925c15a1fdfba624ac85a
Hans Christian Nenseth <nenseth@gmail.com>
2015-01-30 20:04:19 +0100

    oppg a og b

--- index.php
+++ index.php
@@ -10,8 +10,30 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
+        <h1>Obligatorisk oppgave 1<br>Webprogrammering PHP</h1>
+        </br>
+
+        <h3>Oppgave 1</h3><h5>a) For-løkke</h5>
         <?php
-        // put your code here
+        for ($i = 0; $i < 100; $i++) {
+            if ($i % 3 != 0) {
+                $i += 3 - ($i % 3);
+                echo $i . " ";
+            }
+        }
+        ?>
+
+        <h5>b) While-løkke</h5>
+        <?php
+        $a = 0;
+        while ($a < 100) {
+            if ($a % 3 != 0) {
+                $a += 3 - ($a % 3);
+                echo $a . " ";
+            }
+
+            $a++;
+        }
         ?>
     </body>
 </html>
