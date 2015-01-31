a54868a20d9e7ebf857f038ef790eca20dff0400
Hans Christian Nenseth <nenseth@gmail.com>
2015-01-31 20:46:14 +0100

    Assignment no.1 is done

--- Oppgave1.php
+++ Oppgave1.php
@@ -5,21 +5,21 @@
         <meta charset="UTF-8">
         <title>Oblig 1 - Oppgave 1</title>
     </head>
-    
+
     <body>
         <h1>Obligatorisk oppgave 1<br>Webprogrammering PHP</h1>
         </br>
 
         <h3>Oppgave 1</h3>
-        
+
         <p> a) Lag et PHP script som lister ut alle tall som er delelige med 3 opp til 100. Bruk en for-løkke.</br>
             b) Lag samme utskrift som over med en while-løkke.</br>
             c) Finn så gjennomsnittet av de samme tallene (de som er delelig med 3 opptil 100).</br></p>
-        
+
         <h5>a) For-løkke</h5>
-        
+
         <?php
-        for ($i = 0; $i < 100; $i++) {
+        for ($i = 1; $i < 101; $i++) {
             if ($i % 3 == 0) {
                 echo $i . " ";
             }
@@ -27,21 +27,30 @@
         ?>
 
         <h5>b) While-løkke</h5>
-        
+
         <?php
-        $a = 0;
-        while ($a < 100) {
+        $a = 1;
+        while ($a < 101) {
             if ($a % 3 == 0) {
                 echo $a . " ";
             }
             $a++;
         }
         ?>
-        
+
         <h5>c) Gjennomsnittet av overstående tall</h5>
-        
-        
-        
-        
+
+        <?php
+        $m = 0;
+        $n = 0;
+        for ($i = 1; $i < 101; $i++) {
+            if ($i % 3 == 0) {
+                $m += $i;
+                $n++;
+            }
+        }
+        echo "Summen av alle de " . $n . " tallene som er delelig med 3, fra 1 opp til 100, er  " . $m .
+        ". </br>Gjennomsnittet av disse tallene er " . $m / $n;
+        ?>
     </body>
 </html>
