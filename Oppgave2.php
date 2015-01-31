c68fb4aa873d803a68160b3991127dcd6435b8bd
Hans Christian Nenseth <nenseth@gmail.com>
2015-01-31 20:17:47 +0100

    Assignment no.2

---
+++ Oppgave2.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Oblig 1 - Oppgave 2</title>
+    </head>
+    
+    <body>
+        
+        <h3>Oppgave 2</h3>
+        
+        <p> Gitt tallrekken: 1,4,8,1,4,10,5,6,2,4,6. Opprett et array for denne rekken av tall. Bruk så dette arrayet til:</br>
+            a) Skriv ut alle tallene som er over 5.</br>
+            b) Tell opp hvor mange tall som er over 5 og vis dette.</br>
+            c) Liste ut tallene baklengs.</br>
+            d) Finn det minste tallet ved en løkke. Skriv så tallet ut.</br>
+            e) Finn det minste tallet ved en PHP funksjon. Skriv så tallet ut.</br>
+            f) Lag så egendefinerte funksjoner med en innparameter for oppgave a) og b).</br>
+            Parameteren inn skal angi tallet som skal testes på i oppgavene.</br>
+            Funksjonene skal returnere verdier (tips, bruk et array i oppgave a).</br> </p>
+        
+        <h5>a) </h5>
+        
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
