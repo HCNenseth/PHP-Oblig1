16a91b225195d91a0588ca1d18677a1c4d56db1b
HansChristian <HansChristian@10.253.10.231>
2015-02-13 15:55:26 +0100

    Dropdown

---
+++ Konserter.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+
+
+        <form action="" metod="POST">
+            Konsertdatoer:</br>
+            <select size="l" name="dato ">
+                <option>1.Mai</option>
+                <option>5.Mai</option>
+            </select></br>
+        </form>
+        
+        <?php
+        ?>
+    </body>
+</html>
